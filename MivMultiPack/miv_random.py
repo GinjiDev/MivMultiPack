01f8985fe02d1e0d5655fde52fcfaa3d42ae2bcf
Гинджи <nerkotin@gmail.com>
2024-03-24 21:50:19 +0200

    modified:   MivMultiPack/miv_random.py 	modified:   test.py

--- MivMultiPack/miv_random.py
+++ MivMultiPack/miv_random.py
@@ -1,21 +1,17 @@
 import asyncio
 
-class async_random:
-    
+class AsyncRandom:
     def __init__(self, seed=None, digits=8):
-        if seed is None:
-            self.seed = int(asyncio.get_event_loop().time() * 1000)  # Инициализация seed на основе текущего времени
-        else:
-            self.seed = seed
+        self.seed = seed
         self.digits = digits
         self.multiplier = 1664525
         self.increment = 1013904223
         self.modulus = 2**32
 
     async def _generate_random_number(self):
-        
-        #линейный конгруэнтный метод для генерации псевдослучайных чисел
-        
+        current_time = asyncio.get_event_loop().time()
+        if self.seed is None:
+            self.seed = int(current_time * 1000)
         self.seed = (self.multiplier * self.seed + self.increment) % self.modulus
         random_number = self.seed % (10 ** self.digits)
         return random_number
@@ -23,10 +19,10 @@ class async_random:
     async def generate_random_number_async(self):
         return await self._generate_random_number()
 
-    def generate_random_number(self):
+    def generate_random_number_sync(self):
         return asyncio.run(self.generate_random_number_async())
-    
-class async_random_range:
+
+class AsyncRandomRange:
     def __init__(self, start, stop, exclude_bounds=False):
         if start < stop:
             self.start = start
@@ -38,17 +34,13 @@ class async_random_range:
         if self.exclude_bounds:
             self.start += 1
             self.stop -= 1
-        else:
-            self.stop += 1
-        self.seed = int(asyncio.get_event_loop().time() * 1000)
+        current_time = asyncio.get_event_loop().time()
+        self.seed = int(current_time * 1000)
         self.multiplier = 1664525
         self.increment = 1013904223
         self.modulus = 2**32
 
     async def _generate_random_number(self):
-        
-        #линейный конгруэнтный метод для генерации псевдослучайных чисел
-        
         self.seed = (self.multiplier * self.seed + self.increment) % self.modulus
         random_number = self.seed % (self.stop - self.start) + self.start
         return random_number
@@ -56,5 +48,5 @@ class async_random_range:
     async def generate_random_number_async(self):
         return await self._generate_random_number()
 
-    def generate_random_number(self):
+    def generate_random_number_sync(self):
         return asyncio.run(self.generate_random_number_async())
--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-import MivMultiPack, time
+import MivMultiPack, asyncio
 
 module = MivMultiPack
 
-generate = module.async_random()
-
-while True:
-    var = generate.generate_random_number()
+async def main():
+    #генерация случайного числа
+    #seed -> инициализации начального состояния генератора, если не назначено то число генерируется случайно
+    #digits -> управляет количеством цифр в случайном числе
+    generate = module.AsyncRandom(seed=8, digits=3)
+    var = await generate.generate_random_number_async()
     print(var)
-    time.sleep(0.2)
+
+    #генерация случайного диапозонного числа
+    #start -> Начальное число
+    #stop -> Конечное число
+    #exclude_bounds -> определяет, исключать ли граничные значения из диапазона, например при True -39 и 39, при False -40 и 40
+    generate_range = module.AsyncRandomRange(start=-40, stop=40, exclude_bounds=False)
+    var = await generate_range.generate_random_number_async()
+    print(var)
+
+asyncio.run(main())

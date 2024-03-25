6c65822b8ec04fb5e0c6e6cb86f98654de2d5d2a
Гинджи <nerkotin@gmail.com>
2024-03-25 15:36:04 +0200

    modified:   .vscode/PythonImportHelper-v2-Completion.json 	modified:   test.py

--- test.py
+++ test.py
@@ -5,9 +5,7 @@ module = MivMultiPack.AsyncRandom()
 
 
 async def main():
-    while True:
-        random_from_array = await module.get_random_from_array(-10, 10)
-        print("Random number from array:", random_from_array)
-        time.sleep(1)
+    random_from_array = await module.get_random_from_array(-10, 10)
+    print("Random number from array:", random_from_array)
     
 asyncio.run(main())
